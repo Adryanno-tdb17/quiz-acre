1e8d6e723853d2f579dbd73b74c768141c323a4a
Adryanno-tdb17 <adryanno-tdb17@hotmail.com>
2025-09-01 22:23:31 -0500

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,32 @@
-const CACHE_NAME = "quiz-acre-v1";
+const CACHE_NAME = "quiz-acre-v2"; // alterei para v2 para forçar atualização
 const urlsToCache = [
   "/",
   "/index.html",
   "/manifest.json",
   "/icon-192.png",
-  "/icon-512.png"
+  "/icon-512.png",
+  "/fundo.jpg" // 👈 adicionamos o fundo no cache
 ];
 
+// Instalar e salvar no cache
 self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
+// Ativar e limpar caches antigos
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+// Interceptar requisições
 self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request).then(response => {
