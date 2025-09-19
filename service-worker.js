a469fa0e27a67e6415c78d78bb728c40daebf9da
Adryanno-tdb17 <adryanno-tdb17@hotmail.com>
2025-09-18 20:52:56 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -27,4 +27,18 @@ self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request).then(resp => resp || fetch(event.request))
   );
+async function fetchFromCloud() {
+  try {
+    const res = await fetch("perguntas.json", { headers: { "Content-Type": "application/json; charset=utf-8" } });
+    if (!res.ok) throw new Error("Erro ao carregar perguntas.json");
+    const data = await res.json();
+    lessons = data;
+    saveLessons(lessons);
+    renderLessonOptions();
+    alert("Perguntas recarregadas da nuvem!");
+  } catch (err) {
+    console.error(err);
+    alert("Erro ao buscar perguntas da nuvem.");
+  }
+}
 });
